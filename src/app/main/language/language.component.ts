e6691a2cfc64bdcd18d729e92075e4f0402d5ded
Magnus Wolffelt <magnus.wolffelt@gmail.com>
2024-07-02 23:44:33 +0200

    More powerful solution help

--- src/app/main/language/language.component.ts
+++ src/app/main/language/language.component.ts
@@ -3,9 +3,27 @@ import { Component, computed, input, model, signal } from '@angular/core';
 import { toSignal } from '@angular/core/rxjs-interop';
 import { FormsModule } from '@angular/forms';
 
-type LowerCaseString = string;
+type UpperCaseString = string;
 type SingleCharacter = string;
 export type Language = 'swedish' | 'english';
+type WordWithScore = {word: UpperCaseString, score: number};
+
+
+function getWordsWithScores(words: UpperCaseString[], characterFrequencies: {[key: SingleCharacter]: number}, forbiddenCharacters: UpperCaseString): WordWithScore[] {
+  const wordsWithScores: {word: UpperCaseString, score: number}[] = words.map(w => {
+    let score = 0;
+    const seen: {[key: string]: boolean} = {};
+    for(const char of w) {
+      if (seen[char] !== true && !forbiddenCharacters.includes(char)) {
+        score += characterFrequencies[char];
+        seen[char] = true;
+      }
+    }
+    return {word: w, score};
+  });
+  
+  return wordsWithScores;
+}
 
 @Component({
   selector: 'app-language',
@@ -17,20 +35,23 @@ export type Language = 'swedish' | 'english';
 export class LanguageComponent {
   language = input.required<Language>();
   wordLength = input.required<number>();
+  correctCharacters = input.required<string>();
+  presentButWrongAtPosition = input.required<string[]>();
+  knownNotPresent = input.required<string>();
   rawWordBuffer = signal<string | undefined>(undefined);
 
-  allWords = computed<LowerCaseString[]>(() => {
+  allWords = computed<UpperCaseString[]>(() => {
     const rawWordBuffer = this.rawWordBuffer();
     if (rawWordBuffer) {
-      return rawWordBuffer.split(',').map(w => w.toLocaleLowerCase());
+      return rawWordBuffer.split(',').map(w => w.toLocaleUpperCase());
     }
     return [];
   });
 
-  relevantWords = computed<LowerCaseString[]>(() => {
+  relevantWords = computed<UpperCaseString[]>(() => {
     const allWords = this.allWords();
     const wordLength = this.wordLength();
-    return allWords.filter(word => word.length === wordLength && !word.match(/[^a-zåäö]/));
+    return allWords.filter(word => word.length === wordLength && !word.match(/[^A-ZÅÄÖ]/));
   });
 
   characterFrequencies = computed<{[key: SingleCharacter]: number}>(() => {
@@ -49,23 +70,63 @@ export class LanguageComponent {
     return Object.entries(counts).sort((a, b) => b[1] - a[1]);
   });
 
-  topTenStartWords = computed<{word: LowerCaseString, score: number}[]>(() => {
-    const relevantWordsList = this.relevantWords();
+  topStartWords = computed<WordWithScore[]>(() => {
+    const relevantWords = this.relevantWords();
     const characterFrequencies = this.characterFrequencies();
 
-    const wordsWithScores: {word: LowerCaseString, score: number}[] = relevantWordsList.map(w => {
-      let score = 0;
-      const seen: {[key: string]: boolean} = {};
-      for(const char of w) {
-        if (seen[char] !== true) {
-          score += characterFrequencies[char];
-          seen[char] = true;
+    const wordsWithScores = getWordsWithScores(relevantWords, characterFrequencies, '');
+    return wordsWithScores.sort((wws1, wws2) => wws2.score - wws1.score).slice(0, 10);
+  });
+
+  topSecondDiscoveryWords = computed<WordWithScore[]>(() => {
+    const relevantWords = this.relevantWords();
+    const characterFrequencies = this.characterFrequencies();
+    const characterFrequenciesList = this.characterFrequenciesList();
+
+    const forbiddenCharacters = characterFrequenciesList.slice(0, 5).map(entry => entry[0]).join('');
+    console.log("Forbidden characters", forbiddenCharacters);
+
+    const wordsWithScores = getWordsWithScores(relevantWords, characterFrequencies, forbiddenCharacters);
+    return wordsWithScores.sort((wws1, wws2) => wws2.score - wws1.score).slice(0, 10);
+  });
+
+  topGuesses = computed<string[]>(() => {
+    const correctCharacters = this.correctCharacters();
+    const knownNotPresent = this.knownNotPresent();
+    const presentButWrongAtPosition = this.presentButWrongAtPosition();
+    const relevantWords = this.relevantWords();
+    const allKnownPresentCharacters = presentButWrongAtPosition.join('');
+
+    const guesses = relevantWords.filter((word) => {
+      // Remove any word that doesn't have the known present characters
+      for (const char of allKnownPresentCharacters) {
+        if (!word.includes(char)) {
+          return false;
         }
       }
-      return {word: w, score};
+
+      // Remove any word that doesn't fit the correct characters, or includes known not-present characters
+      for (let i=0; i<word.length; i++) {
+        const char = word[i];
+        if (correctCharacters[i] !== ' ' && correctCharacters[i] !== char) {
+          return false;
+        }
+        if (knownNotPresent.includes(char)) {
+          return false;
+        }
+        if (presentButWrongAtPosition[i].includes(char)) {
+          return false;
+        }
+      }
+
+      return true;
     });
-    
-    return wordsWithScores.sort((wws1, wws2) => wws2.score - wws1.score).slice(0, 10);
+
+    if (guesses.length > 40) {
+      return [];
+    } else {
+      return guesses.slice(0, 10);
+    }
   });
 
   constructor(private httpClient: HttpClient) {
--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,4 +1,4 @@
-import { Component, model } from '@angular/core';
+import { Component, computed, model } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { LanguageComponent } from './language/language.component';
 import { FormsModule } from '@angular/forms';
@@ -8,9 +8,48 @@ import { FormsModule } from '@angular/forms';
   standalone: true,
   imports: [RouterLink, LanguageComponent, FormsModule],
   templateUrl: './main.component.html',
-  styleUrl: './main.component.scss'
+  styleUrl: './main.component.scss',
 })
 export class MainComponent {
   wordLength = model(5);
   knownCharacters = model<string[]>();
+
+  // TODO: Support other word lengths
+  correctCharacter1 = model('');
+  correctCharacter2 = model('');
+  correctCharacter3 = model('');
+  correctCharacter4 = model('');
+  correctCharacter5 = model('');
+
+  presentButWrongAtPosition1 = model('');
+  presentButWrongAtPosition2 = model('');
+  presentButWrongAtPosition3 = model('');
+  presentButWrongAtPosition4 = model('');
+  presentButWrongAtPosition5 = model('');
+
+  knownNotPresentInput = model('');
+
+  knownNotPresent = computed<string>(() => {
+    return this.knownNotPresentInput().toLocaleUpperCase().trim();
+  });
+
+  correctCharacters = computed<string>(() => {
+    return [
+      this.correctCharacter1().length > 0 ? this.correctCharacter1() : ' ',
+      this.correctCharacter2().length > 0 ? this.correctCharacter2() : ' ',
+      this.correctCharacter3().length > 0 ? this.correctCharacter3() : ' ',
+      this.correctCharacter4().length > 0 ? this.correctCharacter4() : ' ',
+      this.correctCharacter5().length > 0 ? this.correctCharacter5() : ' ',
+    ].join('').toLocaleUpperCase();
+  });
+
+  presentButWrongAtPosition = computed<string[]>(() => {
+    return [
+      this.presentButWrongAtPosition1(),
+      this.presentButWrongAtPosition2(),
+      this.presentButWrongAtPosition3(),
+      this.presentButWrongAtPosition4(),
+      this.presentButWrongAtPosition5(),
+    ].map(c => c.toLocaleUpperCase().trim());
+  });
 }
